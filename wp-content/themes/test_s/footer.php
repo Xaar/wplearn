737adffe4a838e17821bf2d46133362c5f84f81f
Syed Shah <syed@glassworks.co.uk>
2013-04-19 09:58:47 +0100

    remove site info / credits from footer

--- wp-content/themes/test_s/footer.php
+++ wp-content/themes/test_s/footer.php
@@ -11,12 +11,6 @@
 	</div><!-- #main -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'test_s_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'test_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'test_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'test_s' ), 'Test_s', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
