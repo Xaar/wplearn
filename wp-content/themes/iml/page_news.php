3c5cd398e72bc4db969901ba3e88790e8da3c186
Syed Shah <xaar@outlook.com>
2013-04-24 15:08:20 +0100

    include pods

--- wp-content/themes/iml/page_news.php
+++ wp-content/themes/iml/page_news.php
@@ -12,8 +12,11 @@ define("THISPAGE", "news");
 
 <div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-
-				</div><!-- #content -->
+<?php
+$pods = new Pod('hw_events');
+$pods->findRecords('name ASC', 25);
+echo $pods->showTemplate('tplEvents');
+?>				</div><!-- #content -->
 	</div><!-- #primary -->
 
 
