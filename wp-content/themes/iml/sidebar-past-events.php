15a3dfec0c4d088e77bdc26d0a51271311752019
Syed Shah <xaar@outlook.com>
2013-06-18 10:48:24 +0100

    fixed past events

--- wp-content/themes/iml/sidebar-past-events.php
+++ wp-content/themes/iml/sidebar-past-events.php
@@ -13,10 +13,12 @@ if ( function_exists ( dynamic_sidebar(6) ) ) : ?>
   <div id="upcoming-events">
     <h2>PAST EVENTS</h2>
 <?php
-$wp_query = new WP_Query( array ( 'post_type' => 'news-events', 'posts_per_page' => 6, 'meta_key' => 'event_end_date', 'meta_compare' => '<', 'meta_value' => date('Ymd'), 'orderby' => 'meta_value', 'order' => 'ASC'));
+$wp_query = new WP_Query( array ( 'post_type' => 'news-events', 'posts_per_page' => 6, 'meta_key' => 'event_end_date', 'meta_compare' => '<', 'meta_value' => date('Ymd'), 'orderby' => 'meta_value', 'order' => 'DESC'));
 $i=0;
-while ( $wp_query->have_posts() ) : $wp_query->the_post();
+while ( $wp_query->have_posts() ) : 
+        $wp_query->the_post();
         $i++;
+	if(get_post_meta($post->ID, 'article_type', true)=='News') continue
 ?>
     <div class="event-listing-text-right">
       <h3><?=the_title();?></h3>
