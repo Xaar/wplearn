be0a92939f0f33c2a980df640f5634ffa91566ed
Syed Shah <xaar@outlook.com>
2013-07-02 17:38:21 +0100

    synch

--- wp-content/themes/iml/archive-news-events.php
+++ wp-content/themes/iml/archive-news-events.php
@@ -11,6 +11,7 @@ get_header();
 //$pagex = (get_query_var('page')) ? "?page=".get_query_var('page') : "?page=1";
 $pagex = (get_query_var('page')) ? get_query_var('page') : 1;
 ?>
+<!--<script type="text/javascript" src="<?=get_template_directory_uri();?>/js/jquery.dropkick-1.0.0.js" />-->
 <script>
 var page = <?=$pagex;?>;
 jQuery(document).ready(function($){
